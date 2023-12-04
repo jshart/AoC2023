8928bef7ba72a569ff83106fc32aee11952d1a8d
jshart <jshart@gmail.com>
2023-12-04 19:58:34 +0000

    Update day4_part2.py

--- Day4/day4_part2.py
+++ Day4/day4_part2.py
@@ -6,7 +6,7 @@ class ScratchCard:
         self.cardNumber=0
 
         parts=line.split(":")
-        cardNumber=int(parts[0])
+        self.cardNumber=int(parts[0])
         parts2=parts[1].split("|")
 
         self.addWinningNumbers(parts2[0])
@@ -34,11 +34,7 @@ class ScratchCard:
         for n in self.winningNumbers:
             if n in self.yourNumbers:
                 matches+=1
-        if matches==0:
-            return 0
-        else:
-            matches-=1
-        return(1<<matches)
+        return(matches)
 
 # load a text file
 # read file input.txt into an array of strings
@@ -47,17 +43,24 @@ lines = file1.readlines()
 
 cards=[]
 
+# Build the deck from the input data
 for c in enumerate(lines):
     lines[c[0]]=lines[c[0]].strip()
     print(lines[c[0]])
     cards.append(ScratchCard(lines[c[0]]))
 
-total=0
+# Walk the deck, expanding the cards
 for c in cards:
     c.printCard()
-    total+=c.checkWinningNumbers()
-    print("Matches:"+str(c.checkWinningNumbers()))
+    m=c.checkWinningNumbers()
+    for a in range(c.cardNumber,c.cardNumber+m):
+        cards[a].count+=c.count
 
+# Could do this above, but just to make it easier to read
+# now lets do a second pass counting up the total final cards
+total=0
+for c in cards:
+    print("card:"+str(c.cardNumber)+" has "+str(c.count)+" cards")
+    total+=c.count
 
 print("Total:"+str(total))
-

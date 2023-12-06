c490f0149ba36f3971e19a355d5406885118dd3a
jshart <jshart@gmail.com>
2023-12-06 22:41:58 +0000

    Update day6_part2.py

--- Day6/day6_part2.py
+++ Day6/day6_part2.py
@@ -5,6 +5,26 @@ distance=212206012011044
 #distance=940200
 winCounts=[]
 
+print("---- Searching for left hand border")
+# lets test every button hold time
+for buttonHoldTime in range(0,time+1):
+    timeLeft=time-buttonHoldTime
+    distanceTravelled=timeLeft*buttonHoldTime
+    #print(buttonHoldTime,timeLeft,distanceTravelled)
+    if distanceTravelled>=distance:
+        leftBorder=buttonHoldTime-1
+        break
+
+print("---- Searching for right hand border")
+for buttonHoldTime in range(time,0,-1):
+    timeLeft=time-buttonHoldTime
+    distanceTravelled=timeLeft*buttonHoldTime
+    #print(buttonHoldTime,timeLeft,distanceTravelled)
+    if distanceTravelled>=distance:
+        rightBorder=buttonHoldTime
+        break
 
 # https://www.wolframalpha.com/input?i=solve+d%3D%28t-b%29*b+for+b
-print(distance)
+print(leftBorder,rightBorder)
+newTotal=rightBorder-leftBorder
+print(newTotal)

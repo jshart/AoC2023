bd2a2d08bc335b69fa710641184c7eeb47381989
jshart <jshart@gmail.com>
2023-12-14 23:32:21 +0000

    narrowing in on a solution

--- Day14/day14_part2.py
+++ Day14/day14_part2.py
@@ -6,9 +6,9 @@ file1 = open('Day14/data/input_test.txt', 'r')
 lines = file1.readlines()
 
 class Dish:
-    def __int__(self,map):
+    def __init__(self,m):
         self.map=[]
-        self.copyMap(map)
+        self.copyMap(m)
 
     def copyMap(self,m):
         for i in range(0,len(m)):
@@ -20,105 +20,92 @@ class Dish:
                 print(self.map[i][j],end="")
             print()
 
+    def tiltNorth(self):
+        somethingMoved=True
+        while somethingMoved:
+            somethingMoved=False
+            for i in range(1,len(self.map)):
+                for j in range(0,len(self.map[i])):
+                    if self.map[i][j]=="O":
+                        # is the cell above empty?
+                        if self.map[i-1][j]==".":
+                            # move the rock up
+                            self.map[i-1][j]="O"
+                            self.map[i][j]="."
+                            somethingMoved=True
+
+    def tiltSouth(self):
+        somethingMoved=True
+        while somethingMoved:
+            somethingMoved=False
+            for i in range(len(self.map)-2,-1,-1):
+                for j in range(0,len(self.map[i])):
+                    if self.map[i][j]=="O":
+                        # is the cell above empty?
+                        if self.map[i+1][j]==".":
+                            # move the rock up
+                            self.map[i+1][j]="O"
+                            self.map[i][j]="."
+                            somethingMoved=True
+
+    def tiltEast(self):
+        somethingMoved=True
+        while somethingMoved:
+            somethingMoved=False
+            for j in range(0,len(self.map[0])-1):
+                for i in range(0,len(self.map)):
+                    if self.map[i][j]=="O":
+                        # is the cell above empty?
+                        if self.map[i][j+1]==".":
+                            # move the rock up
+                            self.map[i][j+1]="O"
+                            self.map[i][j]="."
+                            somethingMoved=True
+
+    def tiltWest(self):
+        somethingMoved=True
+        while somethingMoved:
+            somethingMoved=False
+            for j in range(len(self.map[0])-1,0,-1):
+                for i in range(0,len(self.map)):
+                    if self.map[i][j]=="O":
+                        # is the cell above empty?
+                        if self.map[i][j-1]==".":
+                            # move the rock up
+                            self.map[i][j-1]="O"
+                            self.map[i][j]="."
+                            somethingMoved=True
+
+    def compareMaps(self,n):
+        for i in range(0,len(self.map)):
+            for j in range(0,len(self.map[i])):
+                if self.map[i][j]!=n[i][j]:
+                    return False
+        return True
+
+    def runCycle(self):
+        # Tilt North
+        self.tiltNorth()
+        # Tilt West
+        self.tiltWest()
+        # Tilt South
+        self.tiltSouth()
+        # Tilt East
+        self.tiltEast()
+
+    def computeScore(self):
+        totalScore=0
+        # Score the grid
+        for c,m in enumerate(self.map):
+            # how many rocks are in this row?
+            rocks=m.count("O")
+            lineScore=rocks*(len(self.map)-c)
+            totalScore+=lineScore
+
+        return totalScore
 
 map=[]
 
-
-def tiltNorth(map):
-    somethingMoved=True
-    while somethingMoved:
-        somethingMoved=False
-        for i in range(1,len(map)):
-
-            # print("processing:[",end="")
-            # print(map[i],end="")
-            # print("] index="+str(i))
-            # lets check each character in the row
-            # looking for a rock we can move
-            for j in range(0,len(map[i])):
-                if map[i][j]=="O":
-                    # is the cell above empty?
-                    if map[i-1][j]==".":
-                        # move the rock up
-                        map[i-1][j]="O"
-                        map[i][j]="."
-                        somethingMoved=True
-
-def tiltSouth(map):
-    somethingMoved=True
-    while somethingMoved:
-        somethingMoved=False
-        for i in range(len(map)-2,-1,-1):
-
-            # print("processing:[",end="")
-            # print(map[i],end="")
-            # print("] index="+str(i))
-            # lets check each character in the row
-            # looking for a rock we can move
-            for j in range(0,len(map[i])):
-                if map[i][j]=="O":
-                    # is the cell above empty?
-                    if map[i+1][j]==".":
-                        # move the rock up
-                        map[i+1][j]="O"
-                        map[i][j]="."
-                        somethingMoved=True
-
-def tiltEast(map):
-    somethingMoved=True
-    while somethingMoved:
-        somethingMoved=False
-        for j in range(0,len(map[0])-1):
-            # print("processing:[",end="")
-            # print(map[j],end="")
-            # print("] index="+str(j))
-            # lets check each character in the row
-            # looking for a rock we can move
-            for i in range(0,len(map)):
-                if map[i][j]=="O":
-                    # is the cell above empty?
-                    if map[i][j+1]==".":
-                        # move the rock up
-                        map[i][j+1]="O"
-                        map[i][j]="."
-                        somethingMoved=True
-
-def tiltWest(map):
-    somethingMoved=True
-    while somethingMoved:
-        somethingMoved=False
-        for j in range(len(map[0])-1,0,-1):
-            # print("processing:[",end="")
-            # print(map[j],end="")
-            # print("] index="+str(j))
-            # lets check each character in the row
-            # looking for a rock we can move
-            for i in range(0,len(map)):
-                if map[i][j]=="O":
-                    # is the cell above empty?
-                    if map[i][j-1]==".":
-                        # move the rock up
-                        map[i][j-1]="O"
-                        map[i][j]="."
-                        somethingMoved=True
-
-
-def runCycle(map):
-    # Tilt North
-    tiltNorth(map)
-    # Tilt West
-    tiltWest(map)
-    # Tilt South
-    tiltSouth(map)
-    # Tilt East
-    tiltEast(map)
-
-def compareMaps(o,n):
-    for i in range(0,len(o)):
-        for j in range(0,len(o[i])):
-            if o[i][j]!=n[i][j]:
-                return False
-    return True
 
 # Lets  clean up the input data
 for c,l in enumerate(lines):
@@ -127,41 +114,57 @@ for c,l in enumerate(lines):
     map.append(tempMapLine)
     print(map[-1])
 
+originalDish=Dish(map)
+
 print("**** DATA LOAD COMPLETE, starting run")
 print("**** INITIAL MAP - taking a copy ****")
 originalMap=[]
-for i in range(0,len(map)):
-    originalMap.append(map[i].copy())
+for i in range(0,len(originalDish.map)):
+    originalMap.append(originalDish.map.copy())
 
 cycles=1000000000
-#cycles=1
+cycles=15
 c=0
 
-print("MAP compare check:"+str(compareMaps(map,map)))
+dishHistory=[]
+
+print("MAP compare check:"+str(originalDish.compareMaps(originalDish.map)))
 
 while True:
-    runCycle(map)
+    originalDish.runCycle()
     c+=1
-    same=compareMaps(originalMap,map)
+    same=False
+    print("===> Running Cycle:"+str(c)+" Map score:"+str(originalDish.computeScore()))
+    
+    # Lets check the history to see if we have a cycle
+    for historyIndex,h in enumerate(dishHistory):
+        if h.compareMaps(originalDish.map):
+            same=True
+            break
+
     if same:
-        print("**** REACHED SAME MAP **** After:"+str(c))
-        break
+        print("**** REACHED SAME MAP **** After:"+str(c)+" original Map @ "+str(historyIndex))
+        dishHistory[historyIndex].printMap()
+        #break
     else:
         if c % 1000 == 0:
             print("**** MAP IS STILL DIFFERENT **** After:"+str(c))
+
+    dishHistory.append(Dish(originalDish.map))
 
     if c == cycles:
         break
     
 print("**** FINAL MAP ****")
-printMap(map)
+originalDish.printMap()
+print("**** Cycles Remainders:"+str(cycles%c))
 
 totalScore=0
 # Score the grid
-for c,m in enumerate(map):
+for c,m in enumerate(originalDish.map):
     # how many rocks are in this row?
     rocks=m.count("O")
-    lineScore=rocks*(len(map)-c)
+    lineScore=rocks*(len(originalDish.map)-c)
     print("Line "+str(c)+" has "+str(rocks)+" rocks, score="+str(lineScore))
     totalScore+=lineScore
 
@@ -170,3 +173,9 @@ print("Total Score = "+str(totalScore))
 
 # Part 1 answer: 106378
 # didnt cycle back to original map after; After:17953000 cycles
+# After building a dish history stack, it does cycle back to the
+# start after a reasonably small number (~100-150) of cycles, so its
+# viable to mod this.
+# TODO, I need to work out what the remainder is. Need to get my head
+# around the fact that there is a start and end to the cycles that
+# loop.

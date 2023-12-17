7c8768109c0348f360655c7f542e0d9957739526
jshart <jshart@gmail.com>
2023-12-17 21:32:32 +0000

    going wrong!

--- Day17/day17.py
+++ Day17/day17.py
@@ -24,14 +24,22 @@ class Map:
         for c,l in enumerate(lines):
             lines[c]=lines[c].strip()
             tempMapLine=list(lines[c])
+            for c,t in enumerate(tempMapLine):
+                tempMapLine[c]=int(t)
             self.map.append(tempMapLine)
             self.trace.append([0] * len(tempMapLine))
 
-
-        self.row=len(self.map)
-        self.col=len(self.map[0])
         self.printMap("Map Init")
-        self.currentPath=None
+
+        self.startRow=0
+        self.startCol=0
+        self.direction=0
+        self.targetRow=len(self.map)
+        self.targetCol=len(self.map[0])
+
+        self.currentCandidateList=[]
+
+        self.targetFound=False
 
     def printMap(self,s):
         print("** "+s+" **")
@@ -40,32 +48,71 @@ class Map:
                 print(self.map[r][c],end="")
             print()
 
+    def printTrace(self,s):
+        print("** "+s+" **")
+        for r in range(len(self.trace)):
+            for c in range(len(self.trace[r])):
+                print(self.trace[r][c],end="")
+            print()
+
     def initPath(self,sRow,sCol,d,tRow,tCol):
-        self.currentPath=Path(sRow,sCol,d,tRow,tCol)
+        self.startRow=sRow
+        self.startCol=sCol
+        self.direction=d
+        self.targetRow=tRow
+        self.targetCol=tCol
+        self.currentCandidateList.append((self.startRow,self.startCol,self.direction,0))
 
 
-class Path:
-    def __init__(self,startRow,startCol,startDirection,targetRow,targetCol):
-        self.row=startRow
-        self.col=startCol
-        self.direction=startDirection
-        # self.history=[]
-        # self.history.append((self.row,self.col,self.direction))
-        self.targetRow=targetRow
-        self.targetCol=targetCol
+    def stepPath(self):
+        print("*** Current Candidate List: "+str(len(self.currentCandidateList)))
+        tempCandidateList=[]
 
-    def printState(self):
-        print("Row: "+str(self.row)+" col: "+str(self.col)+" direction: "+str(self.direction))
+        # Lets check each candidate in the current list and work out if there
+        # is a valid next step it could do. We work backwards through the list,
+        # so that we can pop the current element from the list before adding
+        # anything new to the list, thus ensuring list integrity
+        for c in self.currentCandidateList:
+            print(c)
 
-    def nextAllowedMoves(self):
+            # set the cell in the tracker to indicate we've hit this cell by adding
+            # the current weight to it
+            self.trace[c[0]][c[1]]=c[3]
+
+            if c[0]==self.targetRow and c[1]==self.targetCol:
+                # we have found the target, lets print out the trace state
+                self.printTrace("Target Found")
+                self.targetFound=True
+
+            newPossibleCandidateDirections=self.potentialAllowedMoves(c)
+            
+            # Lets just save all the new candidates to a list and we'll sort them out
+            # once we're done with the pass of the current candidates
+            for d in newPossibleCandidateDirections:
+                nextStep=self.testMove(c,d)
+                #print("Checking Possible New Candidates:",end="")
+                #print(nextStep)
+                if nextStep==None:
+                    pass
+                else:
+                    if self.trace[nextStep[0]][nextStep[1]]>0:
+                        pass
+                    tempCandidateList.append(nextStep)
+
+        self.currentCandidateList.clear()
+        self.currentCandidateList=tempCandidateList.copy()
+
+
+    def potentialAllowedMoves(self,c):
+        d=c[2] # get the direction of the current candidate
         allowedMoves=[]
         # we are only allowed to move forward (if we've changed direction within the last 2 moves)
         # otherwise we must turn/move into the square to the right or the left. First of all
         # lets work out the left/right options
-        if self.direction==Direction.North or self.direction==Direction.South:
+        if d==Direction.North or d==Direction.South:
             allowedMoves.append(Direction.East)
             allowedMoves.append(Direction.West)
-        elif self.direction==Direction.East or self.direction==Direction.West:
+        elif d==Direction.East or d==Direction.West:
             allowedMoves.append(Direction.North)
             allowedMoves.append(Direction.South)
         
@@ -79,14 +126,14 @@ class Path:
         #         mustChangeDirection=True
         
         # if not mustChangeDirection:
-        allowedMoves.append(self.direction)
+        allowedMoves.append(d)
         
         return allowedMoves
     
-    def testMove(self,map,d):
+    def testMove(self,candidate, d):
 
-        r=self.row
-        c=self.col
+        r=candidate[0]
+        c=candidate[1]
 
         if d==Direction.North:
             r-=1
@@ -97,15 +144,21 @@ class Path:
         elif d==Direction.West:
             c-=1
 
-        if r<0 or r>=len(map):
+        if r<0 or r>=len(self.map):
             return None
-        if c<0 or c>=len(map[r]):
+        if c<0 or c>=len(self.map[r]):
+            return None
+        
+        # Already visited - possible TODO - we might need to rework
+        # this and replace this entry if we think the current one would
+        # be better
+        if self.trace[r][c]!=0:
             return None
 
-        return (r,c,d)
+        return (r,c,d,candidate[3]+self.map[r][c])
     
     def distanceToTarget(self):
-        return abs(self.targetRow-self.row)+abs(self.targetCol-self.col)
+        return abs(self.targetRow-self.startRow)+abs(self.targetCol-self.startCol)
 
 
 
@@ -120,10 +173,13 @@ tRow=len(map.map)-1
 tCol=len(map.map[tRow])
 
 map.initPath(0,0,Direction.East,tRow,tCol)
-moves=map.currentPath.nextAllowedMoves()
-for m in moves:
-    print(str(m),end=",")
-    print(str(map.currentPath.testMove(map.map,m)))
+
+s=0
+maxSteps=600
+while map.targetFound==False and s<maxSteps:    
+    map.stepPath()
+    s+=1
+    map.printTrace("Step "+str(s))
 
 print("**** RUN COMPLETE, final state is:")
 total=0
